5cc64fc1c16b1e18408e9d645c778ef1ea08a77b
Mahima-Suthar <mahimaushaniya@gmail.com>
2022-08-09 14:50:56 +0530

    Initial commit

---
+++ src/app/pages/user/start/start.component.ts
@@ -0,0 +1,67 @@
+import { LocationStrategy } from '@angular/common';
+import { ThisReceiver } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { QuestionService } from 'src/app/services/question.service';
+import { QuizService } from 'src/app/services/quiz.service';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-start',
+  templateUrl: './start.component.html',
+  styleUrls: ['./start.component.css'],
+})
+export class StartComponent implements OnInit {
+  qid;
+  questions:[];
+  
+
+  constructor(
+    private locationSt: LocationStrategy,
+    private _route: ActivatedRoute,
+    private _question: QuestionService,
+    private _quiz: QuizService
+  ) {}
+
+  ngOnInit(): void {
+    this.preventBackButton();
+    this.qid = this._route.snapshot.params.qid;
+    console.log(this.qid);
+    this.loadQuestions();
+  }
+  loadQuestions() {
+    this._question.getQuestionsOfQuizForTest(this.qid).subscribe(
+      (data: any) => {
+        this.questions = data;
+
+        console.log(this.questions);
+      },
+
+      (error) => {
+        console.log(error);
+        Swal.fire('Error', 'Error in loading questions of quiz', 'error');
+      }
+    );
+  }
+
+  preventBackButton() {
+    history.pushState(null, null, location.href);
+    this.locationSt.onPopState(() => {
+      history.pushState(null, null, location.href);
+    });
+  }
+
+  submitQuiz() {
+    Swal.fire({
+      title: 'Do you want to submit the quiz?',
+      showCancelButton: true,
+      confirmButtonText: `Submit`,
+      icon: 'info',
+    }).then((e) => {
+      if (e.isConfirmed) {
+      }
+    });
+  }
+}
+
+
